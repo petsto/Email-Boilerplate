5c304f44fa9b69db40a11ca75b39a2a05dfb5d8c
Peter Stoyanov <ps.phnx@gmail.com>
2016-10-02 23:25:18 +0100

    Gulp and Packages refactoring + Switching from LESS to Stylus
    - Switching LessCSS to Stylus
    - Adding an option for clearing /dist/ folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,98 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
-var gutil = require('gulp-util');
-var autoprefixer = require('gulp-autoprefixer');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
-var less = require('gulp-less');
-var jade = require('gulp-jade');
-var premailer = require('gulp-premailer');
-var browserSync = require('browser-sync');
+
+var gulp     = require('gulp'),
+    plumber  = require('gulp-plumber'),
+    rename   = require('gulp-rename'),
+    gutil    = require('gulp-util'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify       = require('gulp-uglify'),
+    imagemin     = require('gulp-imagemin'),
+    cache        = require('gulp-cache'),
+    stylus       = require('gulp-stylus'),
+    jade         = require('gulp-jade'),
+    clean        = require('gulp-clean'),
+    premailer    = require('gulp-premailer'),
+    browserSync  = require('browser-sync');
+
 
 // For local usage
+// ----------
 gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: "./dist/"
-    }
-  });
+    browserSync({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
 });
+
 
 // Reloading
+// ----------
 gulp.task('bs-reload', function () {
-  browserSync.reload();
+    browserSync.reload();
 });
+
 
 // Optimize Images
+// ----------
 gulp.task('images', function(){
-  gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images/'));
+    gulp.src('src/images/**/*')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images/'));
 });
+
 
 // Convert Jade to HTML
+// ----------
 gulp.task('jade', function(){
-  gulp.src('src/jade/**/*.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('dist/'));
+    gulp.src('src/jade/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('dist/'));
 });
 
-// LESS to CSS
-gulp.task('less', function(){
-  gulp.src(['src/less/*.less'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(less())
-    .pipe(autoprefixer('last 4 versions'))
-    .pipe(gulp.dest('dist/css/'))
-    .pipe(browserSync.reload({stream:true}))
+
+// Stylus to CSS
+// ----------
+gulp.task('stylus', function() {
+    return gulp.src('./src/stylus/styles.styl')
+        .pipe( plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }) )
+        .pipe( stylus() )
+        .pipe( gulp.dest('dist/css/') )
+        .pipe( browserSync.reload( { stream: true } ) );
 });
+
 
 // Building Email
+// ----------
 gulp.task('email', ['images', 'less', 'jade'], function () {
-  gulp.src('./dist/*.html')
-    .pipe(premailer())
-    .pipe(gulp.dest('./dist/email/'));
+    gulp.src('./dist/*.html')
+        .pipe(premailer())
+        .pipe(gulp.dest('./dist/email/'));
 });
 
+
 // Default Task
-gulp.task('default', ['images', 'jade', 'less', 'browser-sync'], function(){
-  gulp.watch("src/less/**/*.less", ['less','bs-reload']);
-  gulp.watch("src/jade/**/*.jade", ['jade','bs-reload']);
-  gulp.watch("src/dist/*.html", ['bs-reload']);
+// ----------
+gulp.task('default', ['images', 'jade', 'stylus', 'browser-sync'], function(){
+    gulp.watch("src/stylus/**/*.styl", ['less','bs-reload']);
+    gulp.watch("src/jade/**/*.jade", ['jade','bs-reload']);
+    gulp.watch("src/dist/*.html", ['bs-reload']);
+});
+
+
+// Clean Dist
+// ----------
+gulp.task('clean', function () {
+    return gulp.src('./dist/', {read: false})
+        .pipe(clean());
 });
